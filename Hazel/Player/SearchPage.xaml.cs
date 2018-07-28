bd1fe96731c1109207234620d87708b87f5db9ee
acoustically <acoustically@kookmin.ac.kr>
2018-07-28 16:46:18 +0900

    add youtube search work

--- Hazel/Player/SearchPage.xaml.cs
+++ Hazel/Player/SearchPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,6 +31,36 @@ namespace Hazel.Player
         private void MusicListImageMouseUp(object sender, MouseButtonEventArgs e)
         {
             MusicListImageClicked?.Invoke(sender, EventArgs.Empty);
+        }
+
+        private void SearchMusicImageMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            string keyword = KeywordTextBox.Text;
+            string key = Preference.youtebeApiKey;
+            string part = "snippet";
+
+            string responseText = "";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BuildQueryaUrl(part, key, keyword));
+            request.Method = "GET";
+            request.Timeout = 10000;
+            using (HttpWebResponse resp = (HttpWebResponse)request.GetResponse())
+            {
+                HttpStatusCode status = resp.StatusCode;
+                Console.WriteLine(status);
+
+                Stream respStream = resp.GetResponseStream();
+                using (StreamReader sr = new StreamReader(respStream))
+                {
+                    responseText = sr.ReadToEnd();
+                }
+            }
+        }
+        private string BuildQueryaUrl(string part, string key, string keyword)
+        {
+            string url = "https://www.googleapis.com/youtube/v3/search";
+            string query = url + "?part=" + part + "&key=" + key + "&q=" + keyword;
+            return query;
         }
     }
 }
---
+++ Hazel/Preference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazel
+{
+    class Preference
+    {
+        public const string youtebeApiKey = "AIzaSyBc6lLc0pwu6cR1dzuxaK5MwEqvla1GT44";
+    }
+}
